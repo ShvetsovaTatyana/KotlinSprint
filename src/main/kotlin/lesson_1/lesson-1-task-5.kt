47bcb480974c295d65a65f42018552e7e9ccb5a8
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-09-15 13:14:19 +0300

    lesson-1-task-5 done

---
+++ src/main/kotlin/lesson_1/lesson-1-task-5.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_1
+
+fun main() {
+    val second: Short = 6480
+    val minuteTotal: Byte = 108
+    val hour: String = "01"
+    val minute: Byte = 48
+    val secondRemainder: String = "00"
+    print("$hour:$minute:$secondRemainder")
+}
