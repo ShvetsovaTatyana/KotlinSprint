7519544c8d99c8a74ed88495edce109a7169548b
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-09-16 21:29:54 +0300

    lesson 2 task_1 done

---
+++ src/main/kotlin/lesson_2/task_1.kt
@@ -0,0 +1,8 @@
+package org.example.lesson_2
+
+fun main() {
+    val totalRatings:Float = 4 * 4f
+    val sum = (3 + 4 + 3 + 5) * 4
+    val arithmeticMean = sum / totalRatings
+    println(arithmeticMean)
+}
