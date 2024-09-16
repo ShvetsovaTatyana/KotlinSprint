40447cb8f9cf8a3495f92fb2f65313166d9db8e3
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-09-16 22:27:22 +0300

    lesson 1 task_5 fixed

--- src/main/kotlin/lesson_1/task_5.kt
+++ src/main/kotlin/lesson_1/task_5.kt
@@ -1,14 +1,11 @@
 package org.example.lesson_1
 
+const val TIMING = 60
 fun main() {
     val second: Int = 6480
-    val minuteTotal: Int = second / 60
-    val hour: Int = minuteTotal / 60
-    val minute: Int = minuteTotal - hour * 60
-    val secondRemainder: Int = second % 60
-    print(String.format("%02d", hour))
-    print(":")
-    print(String.format("%02d", minute))
-    print(":")
-    print(String.format("%02d", secondRemainder))
+    val minuteTotal: Int = second / TIMING
+    val hour: Int = minuteTotal / TIMING
+    val minute: Int = minuteTotal - hour * TIMING
+    val secondRemainder: Int = second % TIMING
+    println("%02d:%02d:%02d".format(hour, minute, secondRemainder))
 }
