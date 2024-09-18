42727d31d318ec0243555121b9416463181cb8f5
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-09-18 22:23:53 +0300

    lesson 2 task_1 fixed

--- src/main/kotlin/lesson_2/task_1.kt
+++ src/main/kotlin/lesson_2/task_1.kt
@@ -1,8 +1,12 @@
 package org.example.lesson_2
 
 fun main() {
-    val totalRatings:Float = 4 * 4f
-    val sum = (3 + 4 + 3 + 5) * 4
-    val arithmeticMean = sum / totalRatings
+    val student = 4
+    val gradeOne = 3
+    val gradeTwo = 4
+    val gradeTree = 3
+    val gradeFour = 5
+    val sum = gradeOne + gradeTwo + gradeTree + gradeFour
+    val arithmeticMean = sum / student.toDouble()
     println(arithmeticMean)
 }
