571790c1ae310ac3a8b3582cbb410e238dedb760
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-09-18 23:09:03 +0300

    lesson 2 task_5 done

---
+++ src/main/kotlin/lesson_2/task_5.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_2
+
+import kotlin.math.pow
+
+fun main() {
+    val initialDepositAmount = 70000
+    val interestRate = 16.7 / 100
+    val years = 20
+    val finalDepositAmount = initialDepositAmount * (1 + interestRate).pow(years)
+    println(String.format("%.3f", finalDepositAmount))
+}
