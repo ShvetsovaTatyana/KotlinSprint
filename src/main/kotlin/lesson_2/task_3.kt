96864cdfb86c2dbc30f08c69fb48fbb52ee676a1
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-09-18 22:39:13 +0300

    lesson 2 task_3 fixed

--- src/main/kotlin/lesson_2/task_3.kt
+++ src/main/kotlin/lesson_2/task_3.kt
@@ -4,11 +4,10 @@ const val MINUTES_IN_HOURS = 60
 fun main() {
     val hour: Int = 9
     val minute: Int = 39
-    val hourToMinutes: Int = (9 * MINUTES_IN_HOURS) + 39
+    val hourToMinutes: Int = hour * MINUTES_IN_HOURS + minute
     val travelTimeMinute: Int = 457
     val justMinutes: Int = hourToMinutes + travelTimeMinute
     val conversionToHours: Int = justMinutes / MINUTES_IN_HOURS
-    val minutesForCalculation = conversionToHours * MINUTES_IN_HOURS
-    val minutesOfArrival = justMinutes - minutesForCalculation
+    val minutesOfArrival = justMinutes % MINUTES_IN_HOURS
     println("$conversionToHours:$minutesOfArrival")
 }
