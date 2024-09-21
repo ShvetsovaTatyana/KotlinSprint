2d5ebb59f68416fe1100a0d4cecbaa35583e1dda
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-09-21 23:27:30 +0300

    lesson 4 task_2 fixed

--- src/main/kotlin/lesson_4/task_2.kt
+++ src/main/kotlin/lesson_4/task_2.kt
@@ -1,13 +1,18 @@
 package org.example.lesson_4
 
+const val WEIGHT_MIN = 35
+const val WEIGHT_MAX = 100
+const val VOLUME_MAX = 100
+
 fun main() {
     val averageWeightOne = 20
     val averageVolumeOne = 80
     val averageWeightTwo = 50
     val averageVolumeTwo = 100
+
     println(
         "Груз с весом 20 кг и объемом 80 л соответствует категории 'Average':" +
-                "${(averageWeightOne > 35) && (averageWeightOne <= 100) && averageVolumeOne < 100}"
+                "${(averageWeightOne > WEIGHT_MIN) && (averageWeightOne <= WEIGHT_MAX) && averageVolumeOne < VOLUME_MAX}"
     )
     println(
         "Груз с весом 50 кг и объемом 100 л соответствует категории 'Average':" +
