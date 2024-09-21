63868c0a4d9710882cd0aab595fb9af5068e5150
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-09-21 23:43:05 +0300

    lesson 4 task_3 fixed

--- src/main/kotlin/lesson_4/task_3.kt
+++ src/main/kotlin/lesson_4/task_3.kt
@@ -1,13 +1,16 @@
 package org.example.lesson_4
 
+const val IS_WEATHER_SUNNY = true
+const val IS_TENT_CURRENTLY_OPEN = true
+const val AIR_HUMIDITY = 20
+const val CURRENT_TIME_OF_YEAR = "зима"
+
 fun main() {
-    val theWeatherTodayIsSunny = true
-    val tentIsCurrentlyOpen = true
-    val airHumidity = 20
-    val currentTimeOfYear = "зима"
+
     println(
         "Благоприятные ли условия сейчас для роста бобовых?${
-            theWeatherTodayIsSunny == true && tentIsCurrentlyOpen == true && airHumidity == 20 && currentTimeOfYear == "не зима"
+            IS_WEATHER_SUNNY == true && IS_TENT_CURRENTLY_OPEN == true &&
+                    AIR_HUMIDITY == 20 && CURRENT_TIME_OF_YEAR != "зима"
         }"
     )
 }
