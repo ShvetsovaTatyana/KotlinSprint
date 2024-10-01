d6b0f6556779e34608a17a5021ed8b04ac78e91a
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-10-01 16:51:18 +0300

    lesson 4 task_3 fixed

--- src/main/kotlin/lesson_4/task_3.kt
+++ src/main/kotlin/lesson_4/task_3.kt
@@ -3,14 +3,22 @@ package org.example.lesson_4
 const val IS_WEATHER_SUNNY = true
 const val IS_TENT_CURRENTLY_OPEN = true
 const val AIR_HUMIDITY = 20
-const val CURRENT_TIME_OF_YEAR = "зима"
+const val CURRENT_TIME_OF_YEAR_SPRING = "весна"
+const val CURRENT_TIME_OF_YEAR_SUMMER = "лето"
+const val CURRENT_TIME_OF_YEAR_AUTUMN = "осень"
 
 fun main() {
+    val isWeatherSunnyNow = true
+    val isTentCurrentlyOpenNow = true
+    val airHumidityNow = 20
+    val currentTimeOfYear = "зима"
 
     println(
         "Благоприятные ли условия сейчас для роста бобовых?${
-            IS_WEATHER_SUNNY == true && IS_TENT_CURRENTLY_OPEN == true &&
-                    AIR_HUMIDITY == 20 && CURRENT_TIME_OF_YEAR != "зима"
+            isWeatherSunnyNow == IS_WEATHER_SUNNY && isTentCurrentlyOpenNow == IS_TENT_CURRENTLY_OPEN &&
+                    airHumidityNow == AIR_HUMIDITY && currentTimeOfYear == CURRENT_TIME_OF_YEAR_SPRING ||
+                    currentTimeOfYear == CURRENT_TIME_OF_YEAR_SUMMER ||
+                    currentTimeOfYear == CURRENT_TIME_OF_YEAR_AUTUMN
         }"
     )
 }
