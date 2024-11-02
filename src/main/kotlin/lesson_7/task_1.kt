125db2a1ffa6720d442fa4efbdc4f3a73e4195df
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-11-02 21:17:09 +0300

    lesson 7 task_1 fixed

--- src/main/kotlin/lesson_7/task_1.kt
+++ src/main/kotlin/lesson_7/task_1.kt
@@ -1,13 +1,11 @@
 package org.example.lesson_7
 
-import kotlin.random.Random
-
 fun main() {
     var password = ""
 
     for (i in 1..3) {
         val characters = 'a'.rangeTo('z').random().toString()
-        val number = Random.nextInt(1, 9).toString()
+        val number = 1.rangeTo(9).random().toString()
         password = password + characters + number
     }
     println(password)
