0d9b272dac11abab05678b60fadbc67512b82af8
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-11-04 13:27:20 +0300

    lesson 8 task_3 fixed

--- src/main/kotlin/lesson_8/task_3.kt
+++ src/main/kotlin/lesson_8/task_3.kt
@@ -6,9 +6,7 @@ fun main() {
     println("Наличие какого ингредиента вы хотите проверить?")
     val ingredient: String = readln()
 
-    if (ingredient.equals(dish[0]) || ingredient.equals(dish[1]) || ingredient.equals(dish[2]) ||
-        ingredient.equals(dish[3]) || ingredient.equals(dish[4])
-    ) {
+    if (ingredient in dish) {
         println("Ингредиент $ingredient в рецепте есть")
         return
     }
