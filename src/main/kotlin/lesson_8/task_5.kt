61ca5641ec1bb13d30887a36691a3b290f243441
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-11-07 12:35:13 +0300

    lesson 8 task_5 fixed

--- src/main/kotlin/lesson_8/task_5.kt
+++ src/main/kotlin/lesson_8/task_5.kt
@@ -2,14 +2,7 @@ package org.example.lesson_8
 
 fun main() {
 
-    var ingredient = ""
     println("Количество ингредиентов:")
-    val quantityOfIngredients = readln().toInt()
-    val arrayOfIngredients: Array<String> = Array(quantityOfIngredients) { "" }
-
-    for (i in 0 until quantityOfIngredients) {
-        ingredient = readln()
-        arrayOfIngredients[i] = ingredient
-    }
-    println(arrayOfIngredients.joinToString(","))
+    val array = Array(readln().toInt()) { readln() }
+    println(array.joinToString(","))
 }
