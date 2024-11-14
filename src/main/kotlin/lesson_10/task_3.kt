6f4abdff9624d01d277baefb399947df607bd7cc
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-11-14 12:59:34 +0300

    lesson 10 task_3 done

---
+++ src/main/kotlin/lesson_10/task_3.kt
@@ -0,0 +1,27 @@
+package org.example.lesson_10
+
+fun main() {
+    println(passwordGeneration(passwordLength()))
+}
+
+fun passwordLength(): Int {
+    val passwordLength = readln().toInt()
+    return passwordLength
+}
+
+fun passwordGeneration(passwordLength: Int): String {
+    var password = ""
+
+    for (i in 1..passwordLength) {
+
+        if (i % 2 != 0) {
+            val numberRandom = 0.rangeTo(9).random()
+            password += numberRandom
+        } else {
+            val charRandom =
+                arrayOf('!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ' ').random()
+            password += charRandom
+        }
+    }
+    return password
+}
