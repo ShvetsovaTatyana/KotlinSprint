6ec17eabc4871ec309134ec987c64163364adffd
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-11-20 20:00:07 +0300

    lesson 11 task_3 at work

--- src/main/kotlin/lesson_11/Room.kt
+++ src/main/kotlin/lesson_11/Room.kt
@@ -1,15 +1,40 @@
 package org.example.lesson_11
 
 fun main() {
+    val room: Room =
+        Room(
+            cover = "ссылка на картинку",
+            name = "Название комнаты",
+            listOfParticipants = "Список участников(ссылки на аватарку)",
+            nickname = "Имя участника",
+            speakerStatus = "Статус говорящего"
+        )
+    val person: Person = Person(
+        cover = "ссылка на картинку",
+        name = "Комната любителей йоги",
+        listOfParticipants = "Список участников(ссылки на аватарку)",
+        nickname = "Таня",
+        speakerStatus = "микрофон выключен"
+    )
 }
+
+class Person(
+    val cover: String,
+    var name: String,
+    val listOfParticipants: String,
+    val nickname: String,
+    var speakerStatus: String,
+) {}
+
 
 class Room(
     val cover: String,
     var name: String,
-    val listOfParticipants: ArrayList<String>,
+    val listOfParticipants: String,
     val nickname: String,
     var speakerStatus: String,
-) {
+
+    ) {
     fun addingAParticipant() {
 
     }
