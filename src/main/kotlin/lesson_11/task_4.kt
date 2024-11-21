546be351d3925fd4e81ace56be121946c66d0ac3
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-11-21 15:15:27 +0300

    lesson 11 task_4 fixed

--- src/main/kotlin/lesson_11/task_4.kt
+++ src/main/kotlin/lesson_11/task_4.kt
@@ -2,14 +2,22 @@ package org.example.lesson_11
 
 class RecipeCategory(
     val category: List<Recipe>,
+    val imageRecipeCategory: String,
+    val nameOfTheRecipeCategory: String,
 )
 
 class Recipe(
     val ingredients: List<Ingredient>,
+    val inFavorite: Boolean,
+    val cookingMethod: String,
+    val imageRecipe: String,
+    val nameOfTheRecipe: String,
+    val portions: Int,
 )
 
 class Ingredient(
     val ingredient: String,
+    val amountOfIngredient: String,
 )
 
 
