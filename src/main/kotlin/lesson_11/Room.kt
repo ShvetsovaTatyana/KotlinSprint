a50989557d4412239fd55303928f48a8530e6fbb
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-11-21 13:04:13 +0300

    lesson 11 task_3 done

--- src/main/kotlin/lesson_11/Room.kt
+++ src/main/kotlin/lesson_11/Room.kt
@@ -12,29 +12,37 @@ fun main() {
         nickname = "Таня",
         speakerStatus = "микрофон выключен"
     )
+    val participant1 = Person("user1", "avatar1.jpg", "разговаривает")
+    val participant2 = Person("user2", "avatar2.jpg", "микрофон выключен")
+
+    room.addPerson(participant1)
+    room.addPerson(participant2)
+    room.updateStatus(participant1.nickname, "пользователь заглушен")
 }
 
 class Person(
     val cover: String,
     val nickname: String,
     var speakerStatus: String,
-) {}
-
+)
 
 class Room(
     val cover: String,
-    var name: String,
+    val name: String,
     val listOfParticipants: MutableList<Person>,
-
-    ) {
-    fun addingAParticipant(person: Person) {
+) {
+    fun addPerson(person: Person) {
         listOfParticipants.add(person)
     }
 
-    fun statusUpdate(nickname: String, speakerStatus: String) {
-        val nicknameNew = listOfParticipants.first { it.nickname == nickname }
-        listOfParticipants.add(nicknameNew)
-        val speakerStatusNew = listOfParticipants.first { it.speakerStatus == speakerStatus }
-        listOfParticipants.add(speakerStatusNew)
+    fun updateStatus(nickname: String, speakerStatus: String) {
+        val nicknameNew = listOfParticipants.find { it.nickname == nickname }
+        if (nicknameNew != null) {
+            listOfParticipants.add(nicknameNew)
+        }
+        val speakerStatusNew = listOfParticipants.find { it.speakerStatus == speakerStatus }
+        if (speakerStatusNew != null) {
+            listOfParticipants.add(speakerStatusNew)
+        }
     }
 }
