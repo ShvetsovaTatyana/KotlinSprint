d243198ab8181515aab900d8005a9c9c7298ce4f
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-11-27 22:12:22 +0300

    lesson 13 task_2 done

--- src/main/kotlin/lesson_13/task_2.kt
+++ src/main/kotlin/lesson_13/task_2.kt
@@ -2,7 +2,7 @@ package org.example.lesson_13
 
 fun main() {
     val person = TelephoneDirectoryTwo("Антон", 89185624512)
-    person.printInformation(person)
+    person.printInformation()
 }
 
 class TelephoneDirectoryTwo(
@@ -10,8 +10,8 @@ class TelephoneDirectoryTwo(
     val phoneNumber: Long,
     val company: String? = null,
 ) {
-    fun printInformation(person: TelephoneDirectoryTwo) {
-        val companyNew = person.company ?: "не указано"
-        println("Имя:${person.name}\nНомер:${person.phoneNumber}\nКомпания:$companyNew")
+    fun printInformation() {
+        val companyNew = company ?: "не указано"
+        println("Имя:${name}\nНомер:${phoneNumber}\nКомпания:$companyNew")
     }
 }
