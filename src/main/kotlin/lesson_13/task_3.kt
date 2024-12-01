fddef2e999d9c7594c6df39a0ab0cda795340958
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-12-01 17:51:03 +0300

    lesson 13 task_3 fixed

--- src/main/kotlin/lesson_13/task_3.kt
+++ src/main/kotlin/lesson_13/task_3.kt
@@ -2,15 +2,13 @@ package org.example.lesson_13
 
 fun main() {
     val personOne = TelephoneDirectoryTree("МТС")
-    val personTwo = TelephoneDirectoryTree("Билайн")
+    val personTwo = TelephoneDirectoryTree("МТС")
     val personTree = TelephoneDirectoryTree(null)
     val personFour = TelephoneDirectoryTree(null)
     val personFive = TelephoneDirectoryTree("null")
     val contactList: List<TelephoneDirectoryTree> = listOf(personOne, personTwo, personTree, personFour, personFive)
-    for (i in contactList) {
-        if (i.company != null)
-            println(i.company)
-    }
+    val contactListNew = contactList.mapNotNull { it.company }.toSet()
+    println(contactListNew)
 }
 
 class TelephoneDirectoryTree(
