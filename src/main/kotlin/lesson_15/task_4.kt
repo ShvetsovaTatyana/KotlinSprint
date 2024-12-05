3eabb5078864ace5a3200cd9e6e2888b808088a8
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-12-06 00:46:56 +0300

    lesson 15 task_4 fixed

--- src/main/kotlin/lesson_15/task_4.kt
+++ src/main/kotlin/lesson_15/task_4.kt
@@ -4,20 +4,19 @@ interface Search {
     fun search(): String
 }
 
-open class MusicStore(
+open class Product(
     val name: String,
     val numberOfUnitsInStock: Int,
-) : Search {
+)
+
+class MusicalInstrument(
+    name: String, numberOfUnitsInStock: Int, val stringInstruments: Int, val windInstruments: Int
+) : Product(name, numberOfUnitsInStock), Search {
     override fun search(): String {
         return "Выполняется поиск"
     }
-
 }
-
-class MusicalInstruments(
-    name: String, numberOfUnitsInStock: Int, val stringInstruments: Int, val windInstruments: Int
-) : MusicStore(name, numberOfUnitsInStock)
 
 class Accessories(
     name: String, numberOfUnitsInStock: Int, val forKeyboardInstruments: String
-) : MusicStore(name, numberOfUnitsInStock)
+) : Product(name, numberOfUnitsInStock)
