2e0b8c812ff992c17f94bae24c46709fc6a151a5
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-12-06 00:40:59 +0300

    lesson 15 task_3 fixed

--- src/main/kotlin/lesson_15/task_3.kt
+++ src/main/kotlin/lesson_15/task_3.kt
@@ -1,8 +1,8 @@
 package org.example.lesson_15
 
 fun main() {
-    val user = Users("Изображение", "Максим", "В сети")
-    val administrator = Administrators("Изображение", "Михаил", "В сети")
+    val user = User("Изображение", "Максим", "В сети")
+    val administrator = Administrator("Изображение", "Михаил", "В сети")
     println(
         "${user.name}-${user.readTheForum()}\n${user.name}-${user.writeMessages()}\n" +
                 "${administrator.name}-${administrator.readTheForum()}\n" +
@@ -12,7 +12,7 @@ fun main() {
     )
 }
 
-abstract class Forum(
+abstract class BaseUser(
     val icon: String,
     val name: String,
     val status: String,
@@ -28,9 +28,9 @@ abstract class Forum(
     }
 }
 
-class Users(icon: String, name: String, status: String) : Forum(icon, name, status)
+class User(icon: String, name: String, status: String) : BaseUser(icon, name, status)
 
-class Administrators(icon: String, name: String, status: String) : Forum(icon, name, status) {
+class Administrator(icon: String, name: String, status: String) : BaseUser(icon, name, status) {
     fun deleteMessages(): String {
         val deleteMessages = "Удалил сообщение"
         return deleteMessages
