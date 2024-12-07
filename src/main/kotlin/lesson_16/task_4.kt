fc97fc556eb065de0c68c8244179f87ac76af36e
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-12-07 23:46:20 +0300

    lesson 16 task_4 fixed

--- src/main/kotlin/lesson_16/task_4.kt
+++ src/main/kotlin/lesson_16/task_4.kt
@@ -1,12 +1,16 @@
 package org.example.lesson_16
 
-class OnlineStore(private val orderNumber: String, var readinessStatus: String) {
+class OnlineStore(private val orderNumber: String, private var readinessStatus: String) {
     private fun changeStatus(newReadinessStatus: String) {
         this.readinessStatus = newReadinessStatus
     }
 
     fun sendAnApplication(newReadinessStatus: String) {
         this.changeStatus(newReadinessStatus)
+    }
+
+    fun getValue(): String {
+        return readinessStatus
     }
 }
 
