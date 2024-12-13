a605bdfa7c675342bb3ae327864b23560aac37de
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-12-14 00:09:13 +0300

    lesson 19 task_3 done

---
+++ src/main/kotlin/lesson_19/task_3.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_19
+
+class Spacecraft() {
+    fun takeOff() {
+        TODO("Здесь нужна информация о дополнительной логике")
+    }
+
+    fun land() {
+        TODO()
+    }
+
+    fun shootBack() {
+    }
+}
