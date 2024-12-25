3a84d589fc0b85ee0d0643f1f3a39184a4a9fe93
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-12-25 22:09:16 +0300

    lesson 20 task_4 fixed

--- src/main/kotlin/lesson_20/task_4.kt
+++ src/main/kotlin/lesson_20/task_4.kt
@@ -3,8 +3,8 @@ package org.example.lesson_20
 fun main() {
     val listOfStrings: List<String> = listOf("сушилка", "стул", "одежда")
     val listOfLambdas = listOfStrings.map { it -> { "Нажат элемент: $it" } }
-    for (i in 0 until listOfLambdas.size) {
-        if (i % 2 == 0)
-            println(listOfLambdas.get(i)())
+    listOfLambdas.forEachIndexed { index, element ->
+        if (index % 2 == 0)
+            println(element())
     }
 }
