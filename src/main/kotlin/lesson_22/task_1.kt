575d261004a843d7f92ebab4f409f66c38c60297
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2025-01-02 15:07:26 +0300

    lesson 22 task_1 done

--- src/main/kotlin/lesson_22/task_1.kt
+++ src/main/kotlin/lesson_22/task_1.kt
@@ -5,7 +5,8 @@ fun main() {
     val bookTwo = RegularBook("Война и мир", "Лев Толстой")
     val bookTree = DataBook("Война и мир", "Лев Толстой")
     val bookFour = DataBook("Война и мир", "Лев Толстой")
-    println("${bookOne == bookTwo}\n${bookTree == bookFour}")
+    println("${bookOne == bookTwo}\n${bookTree == bookFour}") //при сравнении объектов обычных классов приосходит сравнение ссылок,
+// а при сравнении объектов data class сравниваются сами объекты,поэтому и результаты сравнения отличаются
 
 }
 
