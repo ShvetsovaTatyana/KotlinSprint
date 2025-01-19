bfa056446a71f423911ef450da00e17c4356f629
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2025-01-19 23:54:28 +0300

    lesson 22 task_4 fixed

--- src/main/kotlin/lesson_22/task_4.kt
+++ src/main/kotlin/lesson_22/task_4.kt
@@ -1,14 +1,15 @@
 package org.example.lesson_22
 
-class MainScreenViewModel(val mainScreenState: MainScreenState) {
+class MainScreenViewModel(var mainScreenState: MainScreenState) {
     fun loadData() {
-        val lackOfData = MainScreenState("", false)
-        val loadingData = lackOfData.copy(isLoading = true)
-        val downloadedData = loadingData.copy(data = "привет", isLoading = false)
+        mainScreenState = MainScreenState("", false)
+        mainScreenState = mainScreenState.copy(isLoading = true)
+        mainScreenState = mainScreenState.copy(data = "привет", isLoading = false)
     }
+
+    data class MainScreenState(
+        val data: String,
+        val isLoading: Boolean = false,
+    )
 }
 
-data class MainScreenState(
-    val data: String,
-    val isLoading: Boolean = false,
-)
